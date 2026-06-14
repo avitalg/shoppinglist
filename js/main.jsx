a204f898d3f1d84d1c1c2792a380cffbf059ed51
Test <test@example.com>
2026-06-14 14:52:04 +0300

    feat: add vercel analytics

--- js/main.jsx
+++ js/main.jsx
@@ -1,4 +1,5 @@
 import { createRoot } from "react-dom/client";
+import { Analytics } from "@vercel/analytics/react";
 import "./styles/global.css";
 import App from "./App.jsx";
 import ErrorBoundary from "./ErrorBoundary.jsx";
@@ -6,5 +7,6 @@ import ErrorBoundary from "./ErrorBoundary.jsx";
 createRoot(document.getElementById("root")).render(
   <ErrorBoundary>
     <App />
+    <Analytics />
   </ErrorBoundary>
 );
